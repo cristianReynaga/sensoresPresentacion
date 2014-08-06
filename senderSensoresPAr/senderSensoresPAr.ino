189706397dd4cd6fa96829329c5d3f12f4cf34e6
cristianReynaga <cristianreynaga@gmail.com>
2014-08-06 13:49:24 -0300

    first commit

---
+++ senderSensoresPAr/senderSensoresPAr.ino
@@ -0,0 +1,46 @@
+// 
+// Sensor de luz prototipo
+
+#define ARRAY_SIZE 4
+
+int myLed=13;
+int timerOn=800;
+int timerOff=50;
+
+
+int sensorPin[]={
+  A3, A4, A5, A6};
+int valSensor[ARRAY_SIZE];
+int value[ARRAY_SIZE];
+
+void setup() {
+
+  Serial.begin(115200);
+
+  for (int i=0; i<ARRAY_SIZE; i++) {
+    pinMode(sensorPin[i], INPUT);
+  }
+
+}
+
+
+void loop() {
+
+  for (int i=0; i<ARRAY_SIZE; i++) {
+    value[i]=analogRead(sensorPin[i]);
+
+  }
+
+  Serial.print(value[0], DEC);
+  Serial.print(",");
+  Serial.print(value[1], DEC);
+  Serial.print(",");
+  Serial.print(value[2], DEC);
+  Serial.print(",");
+  Serial.print(value[3], DEC);
+  Serial.print(",");
+  Serial.println();
+  delay(50);
+
+}
+
